aea1d931c0f80c5684bcefc7722f94427f63d664
Muneeendra Satti <141901533+Muneendra6305@users.noreply.github.com>
2024-02-28 22:19:44 +0530

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+       Scanner s=new Scanner(System.in);
+       System.out.println("Enter array size:");
+       int n=s.nextInt();
+       int arr[]=new int[n];
+       System.out.println("enter array elements");
+       for(int i=0;i<n;i++)
+       {
+           arr[i]=s.nextInt();
+       }
+       System.out.println("The array elements are");
+      System.out.println(Arrays.toString(arr));
+      System.out.println("After insertion sort the array elements are ");
+
+      for(int i=0;i<n-1;i++)
+      {
+          int min=i;
+          for(int j=i;j<n;j++)
+          {
+          if(arr[j]<arr[min])
+          {
+              min=j;
+          }
+          }
+          int temp=arr[min];
+          arr[min]=arr[i];
+          arr[i]=temp;
+      }
+      System.out.println(Arrays.toString(arr));
+       
+    }
+}
